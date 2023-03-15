93c45f76b0c3242cdce4de1076a9b28465e61775
Kenny Lin <kklin3@uci.edu>
2023-03-14 21:02:57 -0700

    Title updated

--- src/Title.js
+++ src/Title.js
@@ -3,7 +3,9 @@ import './Title.css'
 function Title() {
     return (
         <div className="Title">
-
+            <h1>Eggflation: </h1>
+            <h2>how the pandemic affected eggs</h2>
+            <h3>Kenneth Lin</h3>
         </div>
     );
 }
