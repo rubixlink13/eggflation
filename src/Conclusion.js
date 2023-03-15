a11a5d5e30b5f7a91dbf07a149ac1536b956a29c
Kenny Lin <kklin3@uci.edu>
2023-03-15 05:57:55 -0700

    Update Conclusion

--- src/Conclusion.js
+++ src/Conclusion.js
@@ -1,9 +1,16 @@
 import './Conclusion.css';
 
+const conclusion = "\
+There is no singular factor that created the great Eggflation. \
+Rather, it was a combination of the higher demands and the lower \
+supplies that inevitably drove the prices up, which drove businesses \
+to bankrupcy."
+
 function Conclusion() {
     return (
         <div className="Conclusion">
-
+            <h2>Conclusion</h2>
+            <p>{conclusion}</p>
         </div>
     );
 }
